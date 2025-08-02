cbd4411d8a1027dd0e4198276890350810a35994
Shrishail <shrishailgolappanavar11@gmail.com>
2025-08-02 16:06:25 +0530

    Seperation of Concern

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
+const ownersRouter = require('./routes/owenersRouter');
+const usersRouter = require('./routes/usersRouter');
+const productsRouter = require('./routes/productsRouter');
+
+
+
+const db = require('./config/mongoose-connection');
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +17,10 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs');
+
+app.use('/owners',ownersRouter);
+app.use('/users',usersRouter);
+app.use('/products',productsRouter);
 
 app.get('/',(req,res)=>{
     res.send("Hey");
--- models/ownerModel.js
+++ models/ownerModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema= mongoose.Schema({
+const ownerSchema= mongoose.Schema({
     fullname:{
         type:String,
         minLength:3,
@@ -8,16 +8,12 @@ const userSchema= mongoose.Schema({
     },
     email:String,
     password:String,
-    cart:{
+    products:{
         type:Array,
-        default:[]
+        default:[],
     },
-    orders:{
-        type:Array,
-        default:[]
-    },
-    contact : Number,
+    gstin:Number,
     picture : String,
 })
 
-module.exports = mongoose.model('user',userSchema);
+module.exports = mongoose.model('user',ownerSchema);
