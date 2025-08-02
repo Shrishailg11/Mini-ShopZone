41ece131ba80e028f54bfeabb6e758d23a8642e5
Shrishail <shrishailgolappanavar11@gmail.com>
2025-08-02 17:27:43 +0530

    Development environment and Routes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const config = require('config');
+
+const dbgr = require('debug')('development:mongoose');
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/Mini-ShopZone")
+.connect(`${config.get('MONGODB_URI')}/Mini-ShopZone`)
 .then(function (){
-    console.log("Connected!");
+    dbgr("Connected!");
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 });
 
 module.exports = mongoose.connection;
